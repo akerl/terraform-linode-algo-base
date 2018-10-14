c9d1a0c8ada7c756cf3914362635d3abe0fdf699
Les Aker <me@lesaker.org>
2018-10-15 00:27:52 +0300

    handle repo dir

--- assets/setup.sh
+++ assets/setup.sh
@@ -8,15 +8,18 @@ apt update
 apt upgrade -y
 apt install -y ansible python-pip build-essential python-dev python-virtualenv
 
-local ALGO_DIR=/opt/algo
-
+ALGO_DIR=/opt/algo
 if [[ ! -e "$ALGO_DIR" ]] ; then
     git clone "${algo_repo}" "$ALGO_DIR"
 fi
-
 cd "$ALGO_DIR"
-git pull
 
+if [[ "$(git remote get-url origin)" != "${algo_repo}" ]] ; then
+    echo "Repo in image is not the expected repo"
+    exit 1
+fi
+
+git pull
 [[ ! -e env ]] && python -m virtualenv env
 export PS1=""
 source env/bin/activate
