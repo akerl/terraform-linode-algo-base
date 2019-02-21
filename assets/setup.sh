042607440b4e0699ff57c112996bc5c22d025f4d
Les Aker <me@lesaker.org>
2019-02-21 17:27:20 +0000

    clean up setup script

--- assets/setup.sh
+++ assets/setup.sh
@@ -16,23 +16,3 @@ apt install -y python-pip build-essential python-dev python-virtualenv
 
 systemctl restart sshd
 
-ALGO_DIR=/opt/algo
-if [[ ! -e "$ALGO_DIR" ]] ; then
-    git clone "${algo_repo}" "$ALGO_DIR"
-fi
-
-(
-    cd "$ALGO_DIR"
-
-    if [[ "$(git remote get-url origin)" != "${algo_repo}" ]] ; then
-        echo "Repo in image is not the expected repo"
-        exit 1
-    fi
-
-    git pull
-
-    [[ ! -e env ]] && virtualenv env
-    export PS1=""
-    source env/bin/activate
-    pip install --force-reinstall --no-cache-dir -r requirements.txt
-)
